2fbebf6c4b461c5bdfbff2ef862ee12e449a0c5a
paike <github@pradikow.de>
2025-02-20 16:19:40 +0100

    added eks module output: oidc_provider_arn

--- modules/eks-cluster/outputs.tf
+++ modules/eks-cluster/outputs.tf
@@ -23,6 +23,6 @@ output "cluster_token" {
   sensitive   = true
 }
 
-# output "oidc_provider_arn" {
-#   value = aws_iam_openid_connect_provider.oidc_provider.arn
-# }
+output "oidc_provider_arn" {
+  value = aws_iam_openid_connect_provider.oidc_provider.arn
+}
