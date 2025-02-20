abb4bab318a0d89a9d73b5e79101b92c2088cb25
paike <github@pradikow.de>
2025-02-20 16:14:16 +0100

    ALB: service account and controller will be installed with terraform

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,17 +1,17 @@
 module "lb_role" {
- source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
- role_name                              = "${var.project_id}-${var.environment}-eks-lb-role"
- attach_load_balancer_controller_policy = true
+  role_name                              = "${var.project_id}-${var.environment}-eks-lb-role"
+  attach_load_balancer_controller_policy = true
 
- oidc_providers = {
-     main = {
-     provider_arn               = var.oidc_provider_arn
-     namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
-     }
- }
- }
- 
+  oidc_providers = {
+    main = {
+      provider_arn               = var.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
 data "aws_eks_cluster" "eks_cluster" {
   name = var.cluster_name
 }
@@ -28,63 +28,63 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
-  host                   = var.cluster_endpoint
-  cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
+    host                   = var.cluster_endpoint
+    cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
   }
 }
 
- resource "kubernetes_service_account" "service-account" {
- metadata {
-     name      = "aws-load-balancer-controller"
-     namespace = "kube-system"
-     labels = {
-     "app.kubernetes.io/name"      = "aws-load-balancer-controller"
-     "app.kubernetes.io/component" = "controller"
-     }
-     annotations = {
-     "eks.amazonaws.com/role-arn"               = module.lb_role.iam_role_arn
-     "eks.amazonaws.com/sts-regional-endpoints" = "true"
-     }
- }
- }
+resource "kubernetes_service_account" "service-account" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    labels = {
+      "app.kubernetes.io/name"      = "aws-load-balancer-controller"
+      "app.kubernetes.io/component" = "controller"
+    }
+    annotations = {
+      "eks.amazonaws.com/role-arn"               = module.lb_role.iam_role_arn
+      "eks.amazonaws.com/sts-regional-endpoints" = "true"
+    }
+  }
+}
 
-#  resource "helm_release" "alb-controller" {
-#  name       = "aws-load-balancer-controller"
-#  repository = "https://aws.github.io/eks-charts"
-#  chart      = "aws-load-balancer-controller"
-#  namespace  = "kube-system"
-#  depends_on = [
-#      kubernetes_service_account.service-account
-#  ]
+resource "helm_release" "alb-controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  depends_on = [
+    kubernetes_service_account.service-account
+  ]
 
-#  set {
-#      name  = "region"
-#      value = var.region
-#  }
+  set {
+    name  = "region"
+    value = var.region
+  }
 
-#  set {
-#      name  = "vpcId"
-#      value = var.vpc_id
-#  }
+  set {
+    name  = "vpcId"
+    value = var.vpc_id
+  }
 
-# #  set {
-# #      name  = "image.repository"
-# #      value = "602401143452.dkr.ecr.${var.region}.amazonaws.com/amazon/aws-load-balancer-controller"
-# #  }
+  #  set {
+  #      name  = "image.repository"
+  #      value = "602401143452.dkr.ecr.${var.region}.amazonaws.com/amazon/aws-load-balancer-controller"
+  #  }
 
-#  set {
-#      name  = "serviceAccount.create"
-#      value = "false"
-#  }
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
 
-#  set {
-#      name  = "serviceAccount.name"
-#      value = "aws-load-balancer-controller"
-#  }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
 
-#  set {
-#      name  = "clusterName"
-#      value = var.cluster_name
-#  }
-#  }
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+}
