96d29a927765d8be5abd1777efd3b3b51d63f627
paike <github@pradikow.de>
2025-02-24 14:08:44 +0100

    debugging logging module [deploy]

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -1,3 +1,13 @@
+output "oidc_provider_url" {
+  value = var.oidc_provider_url
+}
+
+output "oidc_provider_arn" {
+  value = var.oidc_provider_url
+}
+
+
+
 data "aws_iam_policy_document" "eks_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -22,7 +32,7 @@ resource "aws_iam_role" "eks_service_account" {
 }
 
 resource "aws_iam_role_policy" "cloudwatch_logging" {
-  name   = "${var.project_id}-${var.environment}-eks-cloud-watch-logging-role"
+  name   = "${var.project_id}-${var.environment}-eks-cloud-watch-logging-policy"
   role   = aws_iam_role.eks_service_account.name
   policy = jsonencode({
     Version = "2012-10-17",
@@ -45,14 +55,9 @@ resource "kubernetes_service_account" "cloudwatch_agent" {
   metadata {
     name      = "cloudwatch-agent"
     namespace = var.environment
-    labels = {
-      "app.kubernetes.io/name"      = "cloudwatch-agent"
-      "app.kubernetes.io/component" = "agent"
-    }
 
     annotations = {
       "eks.amazonaws.com/role-arn" = aws_iam_role.eks_service_account.arn
-      "eks.amazonaws.com/sts-regional-endpoints" = "true"
     }
   }
 }
