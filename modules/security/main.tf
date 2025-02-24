3344f2a723c3fe19777e71d185bd318a63a9aaee
paike <github@pradikow.de>
2025-02-24 15:15:10 +0100

    renamed the resource node_cloud_watch_policy [deploy]

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -41,7 +41,7 @@ resource "aws_iam_role_policy_attachment" "node_registry_policy" {
 }
 
 # Attach the CloudWatchAgentServerPolicy for sending to CloudWatch
-resource "aws_iam_role_policy_attachment" "node_worker_policy" {
+resource "aws_iam_role_policy_attachment" "node_cloud_watch_policy" {
   role       = aws_iam_role.node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
