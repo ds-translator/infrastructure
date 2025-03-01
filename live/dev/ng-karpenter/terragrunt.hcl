d2e687cb153d73fe8b2559eaa076d77090960537
paike <github@pradikow.de>
2025-03-01 17:07:39 +0100

    added nodegroup for karpenter

---
+++ live/dev/ng-karpenter/terragrunt.hcl
@@ -0,0 +1,58 @@
+include {
+  path = find_in_parent_folders("root.hcl")
+}
+
+dependency "security" {
+  config_path = "../security"
+
+  mock_outputs = {
+    node_role_name = "mock-eks-node-group-role"
+    node_role_arn  = "arn:aws:iam::123456789012:role/mock-eks-node-group-role"
+  }  
+}
+
+dependency "networking" {
+  config_path = "../networking"
+
+  mock_outputs = {
+    private_subnets = ["subnet-12345678", "subnet-87654321"]
+  }    
+}
+
+dependency "eks_cluster" {
+  config_path = "../eks-cluster"
+
+  mock_outputs = {
+    cluster_name = "dst-dev-cluster"
+  }
+}
+
+terraform {
+  source = "../../../modules/eks-node-group///"
+}
+
+inputs = {
+  node_role_arn    = dependency.security.outputs.node_role_arn
+  cluster_name     = dependency.eks_cluster.outputs.cluster_name 
+  
+  node_group_name  = "frontend"
+  subnet_ids       = dependency.networking.outputs.private_subnets
+  desired_size     = 2
+  min_size         = 1
+  max_size         = 1
+  instance_types   = ["t2.micro"]
+  disk_size        = 20
+  ec2_ssh_key      = ""                
+  source_security_group_ids = []        
+  max_unavailable  = 1
+
+ # Enable taints and provide the taint details
+  enable_taints = true
+  node_taints = [
+    {
+      key    = "karpenter"
+      value  = "true"
+      effect = "NO_SCHEDULE"
+    }
+  ]  
+}
--- modules/eks-node-group/main.tf
+++ modules/eks-node-group/main.tf
@@ -15,30 +15,39 @@ resource "aws_eks_node_group" "this" {
   }
 
   launch_template {
-    name      = "${var.project_id}-eks-node-template"
-    version = "$Latest" 
+    name    = "${var.project_id}-eks-node-template"
+    version = "$Latest"
   }
 
   instance_types = var.instance_types
   # disk_size      = var.disk_size
 
   # (Optional) Enable remote access for SSH into worker nodes.
-#   dynamic "remote_access" {
-#     for_each = var.ec2_ssh_key != "" && length(var.source_security_group_ids) > 0 ? [1] : []
-#     content {
-#       ec2_ssh_key               = var.ec2_ssh_key
-#       source_security_group_ids = var.source_security_group_ids
-#     }
-#   }
+  #   dynamic "remote_access" {
+  #     for_each = var.ec2_ssh_key != "" && length(var.source_security_group_ids) > 0 ? [1] : []
+  #     content {
+  #       ec2_ssh_key               = var.ec2_ssh_key
+  #       source_security_group_ids = var.source_security_group_ids
+  #     }
+  #   }
 
   update_config {
     max_unavailable = var.max_unavailable
   }
 
-  tags = {
-      "Environment" = var.environment,
-      "Project"     = var.project_name
-      "Name" = "${var.project_id}-${var.environment}-eks-node-frontend"
-      "NodeGroup"   = var.node_group_name
+  dynamic "taints" {
+    for_each = var.enable_taints ? var.node_taints : []
+    content {
+      key    = taints.value.key
+      value  = taints.value.value
+      effect = taints.value.effect
     }
+  }
+
+  tags = {
+    "Environment" = var.environment,
+    "Project"     = var.project_name
+    "Name"        = "${var.project_id}-${var.environment}-eks-node-frontend"
+    "NodeGroup"   = var.node_group_name
+  }
 }
